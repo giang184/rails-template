7664485358d751058f87c82ca591aa3247e42e9e
Andrew Giang <giang184@gmail.com>
2021-08-04 08:30:45 -0700

    add spec files for artist

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Album do
   it { should have_many(:songs) }
   it { should validate_presence_of :name }
+  it { should have_and_belong_to_many :artists }
   it { should validate_length_of(:name).is_at_most(100) }
   it("titleizes the name of an album") do
     album = Album.create({name: "giant steps", genre: "jazz"})
